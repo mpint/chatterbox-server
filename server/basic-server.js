57cd9247660643e63dee54deca7b0a7eeaa06cfc
Michael Pinter <mpinter09@gmail.com>
2014-09-29 14:21:02 -0700

    set up server.js

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,13 +1,12 @@
 /* Import node's http module: */
 var http = require("http");
-
+var handler = require('./request-handler.js')
 
 /* Every server needs to listen on a port with a unique number. The
  * standard port for HTTP servers is port 80, but that port is
  * normally already claimed by another server and/or not accessible
  * so we'll use a higher port number that is not likely to be taken: */
 var port = 3000;
-
 /* For now, since you're running this server on your local machine,
  * we'll have it listen on the IP address 127.0.0.1, which is a
  * special address that always refers to localhost. */
@@ -19,7 +18,7 @@ var ip = "127.0.0.1";
 we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
 will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
 Lastly, we tell the server we made to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
