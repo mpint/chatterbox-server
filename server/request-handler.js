b87b3d6eca2ae2adaea5fc563e9c1703e515317b
mpint <mpinter09@gmail.com>
2014-09-29 19:49:27 -0700

    finished first seven tests (like two bosses)

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,6 +4,7 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var data = [];
 
 exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
@@ -13,60 +14,61 @@ exports.handleRequest = function(request, response) {
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
   console.log("Serving request type " + request.method + " for url " + request.url);
-    // console.log(request);
-console.log('TEST')
-  // {results:[{username: 'Jono', message:'Do my bidding!'}]}
-  var data  = {results:[]};
-  if (request.method === 'GET') {
-    var statusCode = 200;
-  }
-  if (request.method === 'POST') {
-    var statusCode = 201;
-    var body = '';
-    request.on('data', function (chunk) {
-      body += chunk;
-      console.log(body);
-    });
 
-    request.on('end', function() {
-      var parsed = JSON.parse(body);
-      data.results.unshift(parsed);
-      console.log(data.results);
-    });
-
-    // data.results.unshift(parsed);
+  if (request.method === 'GET') { // requests all messages
+    if (request.url === '/classes/messages') {
+      console.log("404?");
+      var statusCode = 200;
+      var headers = defaultCorsHeaders;
+      headers['Content-Type'] = "application/json";
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify({results:data}));
+    } else {
+      var statusCode = 404;
+      var headers = defaultCorsHeaders;
+      headers['Content-Type'] = "text/plain";
+      response.writeHead(statusCode, headers);
+      response.end("404 Not Found");
+    }
   }
 
-  // http.createServer(function (req, res) {
-  //   var body = "";
-  //   req.on('data', function (chunk) {
-  //     body += chunk;
-  //   });
-  //   req.on('end', function () {
-  //     console.log('POSTed: ' + body);
-  //     res.writeHead(200);
-  //     res.end(postHTML);
-  //   });
-  // }).listen(8080);
+  if (request.method === 'POST') { // adds message to data
+    if (request.url === '/classes/messages') {
+      var statusCode = 201;
+      var body = '';
+      response.writeHead(statusCode, headers);
+      request.on('data', function (chunk) {
+        body += chunk;
+      });
 
+      request.on('end', function() {
+        var parsed = JSON.parse(body);
+        data.unshift(parsed);
+
+        var headers = defaultCorsHeaders;
+        headers['Content-Type'] = "application/json";
+        response.writeHead(statusCode, headers);
+        response.end(JSON.stringify({results: data}));
+      });
+  } else {
+      var statusCode = 404;
+      var headers = defaultCorsHeaders;
+      headers['Content-Type'] = "text/plain";
+      response.writeHead(statusCode, headers);
+      response.end("404 Not Found");
+    }
+  }
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
-  var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "application/json";
 
   /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
 
-
-
-  var stringifiedData = JSON.stringify(data);
 
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end(stringifiedData);
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
