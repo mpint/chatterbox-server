6e9daec84964982b00b7ef5e225bba5e715dbb89
mpint <mpinter09@gmail.com>
2014-09-29 18:06:21 -0700

    got a few things working

--- server/request-handler.js
+++ server/request-handler.js
@@ -13,23 +13,60 @@ exports.handleRequest = function(request, response) {
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
   console.log("Serving request type " + request.method + " for url " + request.url);
+    // console.log(request);
+console.log('TEST')
+  // {results:[{username: 'Jono', message:'Do my bidding!'}]}
+  var data  = {results:[]};
+  if (request.method === 'GET') {
+    var statusCode = 200;
+  }
+  if (request.method === 'POST') {
+    var statusCode = 201;
+    var body = '';
+    request.on('data', function (chunk) {
+      body += chunk;
+      console.log(body);
+    });
 
-  var statusCode = 200;
+    request.on('end', function() {
+      var parsed = JSON.parse(body);
+      data.results.unshift(parsed);
+      console.log(data.results);
+    });
+
+    // data.results.unshift(parsed);
+  }
+
+  // http.createServer(function (req, res) {
+  //   var body = "";
+  //   req.on('data', function (chunk) {
+  //     body += chunk;
+  //   });
+  //   req.on('end', function () {
+  //     console.log('POSTed: ' + body);
+  //     res.writeHead(200);
+  //     res.end(postHTML);
+  //   });
+  // }).listen(8080);
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
   var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
 
   /* .writeHead() tells our server what HTTP status code to send back */
   response.writeHead(statusCode, headers);
+
+
+
+  var stringifiedData = JSON.stringify(data);
 
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+  response.end(stringifiedData);
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
